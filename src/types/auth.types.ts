3e75d64d519a22f1e5fffb1e2a74727dc13588b7
cecil <ola@example.com>
2025-10-08 13:16:11 +0200

    day-3/User authentication using JWT

---
+++ src/types/auth.types.ts
@@ -0,0 +1,24 @@
+export interface RegisterDTO {
+  email: string;
+  name: string;
+  password: string;
+}
+
+export interface LoginDTO {
+  email: string;
+  password: string;
+}
+
+export interface AuthResponse {
+  token: string;
+  user: {
+    id: string;
+    email: string;
+    name: string;
+  };
+}
+
+export interface JWTPayload {
+  userId: string;
+  email: string;
+}
